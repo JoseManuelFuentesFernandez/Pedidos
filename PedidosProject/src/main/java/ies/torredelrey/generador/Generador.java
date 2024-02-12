01894e7786886b956980c712f667a9d98d1540b9
jmfuentesfernandez <fuentesfernandezjosemanuel@gmail.com>
2024-02-12 09:29:50 +0100

    Implementacion Clase Generadora de informes + carpetas Resources

---
+++ PedidosProject/src/main/java/ies/torredelrey/generador/Generador.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ies.torredelrey.generador;
+
+/**
+ *
+ * @author usuario
+ */
+public class Generador {
+    //////////////////////////////  PLANTILLA NO TOCAR  //////////////////////////////
+    /*public static void leerInforme(List<> lista, String rutaInforme, String nombreInformeSalida){
+<<<<<<< HEAD
+        try {
+            JasperPrint print;
+            HashMap param = new HashMap();
+            param.put("fecha", LocalDate.now().toString());
+
+            JRDataSource datasource = new JRBeanArrayDataSource(lista.toArray());
+            print = JasperFillManager.fillReport(rutaInforme, param,datasource);
+            JasperExportManager.exportReportToPdfFile(print,nombreInformeSalida);
+            JasperViewer.viewReport(print);
+        } catch (JRException ex) {
+            Logger.getLogger(Generador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }*/
+    
+    ///////////////////////////////////////////////////////////////////////////////////
+    // ****************** CODIFICAR AQUI ABAJO ******************
+    
+    
+}
