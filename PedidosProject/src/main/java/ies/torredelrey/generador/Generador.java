add6241a122e1209e107a0bba0c4300a3725ab74
Driusito <driujuegos@gmail.com>
2024-02-13 12:30:52 +0100

    Actualizacion diseño
    Botones productos, empleados

--- PedidosProject/src/main/java/ies/torredelrey/generador/Generador.java
+++ PedidosProject/src/main/java/ies/torredelrey/generador/Generador.java
@@ -4,6 +4,18 @@
  */
 package ies.torredelrey.generador;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+
 /**
  *
  * @author usuario
@@ -28,6 +40,37 @@ public class Generador {
     
     ///////////////////////////////////////////////////////////////////////////////////
     // ****************** CODIFICAR AQUI ABAJO ******************
-    
+     public static void leerInformeProductos(Collection listaProductos,String rutaInforme,String nombreInformeSalida){
+        try {
+            
+            JasperPrint print;
+            HashMap param = new HashMap();
+            param.put("categoriaid", 100);
+            param.put("total", listaProductos.size());
+            JRDataSource datasource = new JRBeanArrayDataSource(listaProductos.toArray());
+            print = JasperFillManager.fillReport(rutaInforme, param,datasource);
+            JasperExportManager.exportReportToPdfFile(print,nombreInformeSalida);
+            JasperViewer.viewReport(print);
+        } catch (JRException ex) {
+            Logger.getLogger(Generador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+      public static void leerInformeEmpleados(Collection listaEmpleados,String rutaInforme,String nombreInformeSalida){
+        try {
+            
+            JasperPrint print;
+            HashMap param = new HashMap();
+            param.put("categoriaid", 100);
+            param.put("total", listaEmpleados.size());
+            JRDataSource datasource = new JRBeanArrayDataSource(listaEmpleados.toArray());
+            print = JasperFillManager.fillReport(rutaInforme, param,datasource);
+            JasperExportManager.exportReportToPdfFile(print,nombreInformeSalida);
+            JasperViewer.viewReport(print);
+        } catch (JRException ex) {
+            Logger.getLogger(Generador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+     
     
 }
