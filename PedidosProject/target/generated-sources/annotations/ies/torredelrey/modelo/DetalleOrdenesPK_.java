d86e4e97fc2986502a7f14b3491ec9f658df8171
Driusito <driujuegos@gmail.com>
2024-02-13 12:34:41 +0100

    0.1

---
+++ PedidosProject/target/generated-sources/annotations/ies/torredelrey/modelo/DetalleOrdenesPK_.java
@@ -0,0 +1,14 @@
+package ies.torredelrey.modelo;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-13T10:32:13", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(DetalleOrdenesPK.class)
+public class DetalleOrdenesPK_ { 
+
+    public static volatile SingularAttribute<DetalleOrdenesPK, Integer> ordenid;
+    public static volatile SingularAttribute<DetalleOrdenesPK, Integer> detalleid;
+
+}
