8ffeee5711f98a76fb63b6dffa975febb956f5e3
Manuel Alejandro Márquez <manuelalejandromarquezlopez@iestorredelrey.es>
2024-02-16 10:15:40 +0100

    Implementación Pedidos

--- PedidosProject/target/generated-sources/annotations/ies/torredelrey/modelo/Ordenes_.java
+++ PedidosProject/target/generated-sources/annotations/ies/torredelrey/modelo/Ordenes_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-14T12:26:41", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-16T08:50:25", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Ordenes.class)
 public class Ordenes_ { 
 
